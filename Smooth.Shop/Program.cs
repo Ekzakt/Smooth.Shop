558d4a99c6225745e70a6c4089e036c7245564ca
EricJansen <EricJansen@LAPTOP-EJ>
2024-07-17 20:03:02 -0400

    Attempt to fix SSO in Google Chrome.

--- Smooth.Shop/Program.cs
+++ Smooth.Shop/Program.cs
@@ -1,4 +1,5 @@
-using Smooth.Shop.Application.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Smooth.Shop.Bogus;
 
 namespace Smooth.Shop
@@ -19,14 +20,21 @@ namespace Smooth.Shop
 
             builder.Services.AddAuthentication(options =>
             {
-                options.DefaultScheme = "Cookies";
-                options.DefaultChallengeScheme = "oidc";
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
             })
-            .AddCookie("Cookies", options =>
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
             {
-                options.Cookie.Name = "Smooth.Shop";
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.Name = ".AspNet.SharedCookie";
+
+                if (builder.Environment.IsDevelopment())
+                {
+                    options.Cookie.Domain = "localhost";
+                }
             })
-            .AddOpenIdConnect("oidc", options =>
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
             {
                 options.Authority = builder.Configuration.GetValue<string>("IdentityServer:Authority");
                 options.ClientId = builder.Configuration.GetValue<string>("IdentityServer:ClientId");
@@ -34,12 +42,15 @@ namespace Smooth.Shop
 
                 options.ResponseType = "code";
                 options.ResponseMode = "query";
-                
+
                 options.Scope.Clear();
                 options.Scope.Add("openid");
                 options.Scope.Add("profile");
+                options.Scope.Add("offline_access");
                 options.Scope.Add("flauntapi.read");
-                
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.UsePkce = true;
+
                 options.MapInboundClaims = false; // Don't rename claim types
 
                 options.SaveTokens = true;
