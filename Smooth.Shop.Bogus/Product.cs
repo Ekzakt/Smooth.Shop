823a697a7ef33ccfb97c6c00e027720b2f446f94
EricJansen <EricJansen@LAPTOP-EJ>
2024-08-12 18:51:03 -0400

    #nullable disable for Product class.

--- Smooth.Shop.Bogus/Product.cs
+++ Smooth.Shop.Bogus/Product.cs
@@ -1,13 +1,14 @@
-namespace Smooth.Shop.Bogus
+namespace Smooth.Shop.Bogus;
+
+#nullable disable
+
+public class Product
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-        public string Category { get; set; }
-        public string ImageUrl { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string Category { get; set; }
+    public string ImageUrl { get; set; }
 }
