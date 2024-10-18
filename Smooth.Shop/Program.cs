4874ac333cbd9492677c56ab060a2d399977c5d5
EricJansen <EricJansen@LAPTOP-EJ>
2024-10-18 21:59:59 +0200

    Fixed sso setup.

--- Smooth.Shop/Program.cs
+++ Smooth.Shop/Program.cs
@@ -1,94 +1,106 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Logging;
 using Smooth.Shop.Data;
 using Smooth.Shop.FakeData;
 
-namespace Smooth.Shop
+namespace Smooth.Shop;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var environment = builder.Environment;
+
+        IdentityModelEventSource.ShowPII = environment.IsDevelopment(); ;
+
+        builder.Services.AddSingleton<ProductData>();
+
+        builder.Services.Configure<ForwardedHeadersOptions>(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var environment = builder.Environment;
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+        });
 
-            IdentityModelEventSource.ShowPII = environment.IsDevelopment(); ;
+        builder.Services.Configure<RouteOptions>(routeOptions =>
+        {
+            routeOptions.LowercaseUrls = true;
+        });
 
-            builder.Services.AddSingleton<ProductData>();
-            builder.Services.Configure<RouteOptions>(routeOptions =>
-            {
-                routeOptions.LowercaseUrls = true;
-            });
+        builder.Services.AddControllersWithViews();
 
-            builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+        builder.Services.AddDataProtection()
+            .PersistKeysToDbContext<ApplicationDbContext>()
+            .SetApplicationName("SmoothSensation.SharedCookie");
 
-            builder.Services.AddDataProtection()
-                .PersistKeysToDbContext<ApplicationDbContext>()
-                .SetApplicationName("SmoothSensation.SharedCookie");
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+        .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+        {
+            options.Cookie.Name = ".AspNet.SharedCookie";
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.Cookie.Domain = builder.Configuration["IdentityServer:CookieDomain"];
+            options.Cookie.HttpOnly = true;
+            options.Cookie.Path = "/";
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+            options.SlidingExpiration = true;
+        })
+        .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+        {
+            options.Authority = builder.Configuration["IdentityServer:Authority"];
+            options.ClientId = builder.Configuration["IdentityServer:ClientId"];
+            options.ClientSecret = builder.Configuration["IdentityServer:ClientSecret"];
+            options.RequireHttpsMetadata = !environment.IsDevelopment();
+            options.SaveTokens = true;
+            options.ResponseType = "code";
+            options.ResponseMode = "query";
+            options.GetClaimsFromUserInfoEndpoint = true;
+            options.UsePkce = true;
+            options.MapInboundClaims = false;
 
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            })
-            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-            {
-                options.Cookie.Name = ".AspNet.SharedCookie";
-                options.Cookie.SameSite = SameSiteMode.None;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                options.Cookie.Domain = builder.Configuration["IdentityServer:CookieDomain"];
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
-                options.SlidingExpiration = true;
-            })
-            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-            {
-                options.Authority = builder.Configuration["IdentityServer:Authority"];
-                options.ClientId = builder.Configuration["IdentityServer:ClientId"];
-                options.ClientSecret = builder.Configuration["IdentityServer:ClientSecret"];
-                options.RequireHttpsMetadata = !environment.IsDevelopment();
-                options.SaveTokens = true;
-                options.ResponseType = "code";
-                options.ResponseMode = "query";
-                options.GetClaimsFromUserInfoEndpoint = true;
-                options.UsePkce = true;
-                options.MapInboundClaims = false;
+            options.Scope.Clear();
+            options.Scope.Add("openid");
+            options.Scope.Add("profile");
+            options.Scope.Add("offline_access");
+            options.Scope.Add("flauntapi.read");
+        });
 
-                options.Scope.Clear();
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.Scope.Add("offline_access");
-                options.Scope.Add("flauntapi.read");
-            });
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+        app.UseForwardedHeaders();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            app.UseAuthorization();
-
-            app
-                .MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+        app.UseAuthorization();
+
+        app
+            .MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
