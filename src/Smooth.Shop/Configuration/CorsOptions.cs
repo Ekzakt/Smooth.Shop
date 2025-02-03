93e2ba355f47e72f5d8285a6b7f2b72329e03028
Eric Jansen <dev@ekzakt.be>
2025-02-03 09:13:15 +0100

    Added CORS.

---
+++ src/Smooth.Shop/Configuration/CorsOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Smooth.Shop.Configuration;
+
+public class CorsOptions
+{
+    public const string SectionName = "Cors";
+
+    public const string POLICY_NAME = "AllowedOrigins";
+
+    public string[] AllowedOrigins { get; init; } = Array.Empty<string>();
+}
